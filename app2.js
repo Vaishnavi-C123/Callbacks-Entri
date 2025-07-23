05ecc0486b91e878c7a6ee14887ba31b3818d2ed
Vaishnavi C <vaishnavivaishu6555@gmail.com>
2025-07-23 20:21:18 +0530

    Create app2.js

---
+++ app2.js
@@ -0,0 +1,14 @@
+function abc(){
+    console.log("I am inside abc")
+}
+
+console.log("Hello")
+let interval = setInterval(abc, 1000)
+console.log("Hi")
+
+function clear()
+{
+    clearInterval(interval)
+}
+
+let timeout  = setTimeout(clear, 3000)
